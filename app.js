1126424b2fbc6d3ce1e95506c155ed185a01edb9
john0doh <jod18045@gmail.com>
2021-01-09 22:20:45 +0000

    Edits to app and redo of package

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var dt = require('./module.js');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write("The date and time are: " + dt.myDateTime());
   res.end();
-}).listen(8080); 
+}).listen(5000); 
